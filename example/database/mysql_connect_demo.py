d3c9594fd3506884f412a7382415e3ca4742971b
ByDylan-YH <921644606@qq.com>
2021-07-23 11:06:13 +0800

    code review

--- example/database/mysql_connect_demo.py
+++ example/database/mysql_connect_demo.py
@@ -1,23 +1,32 @@
 # pip3 install mysql-connector-python --allow-external mysql-connector-python
 import mysql.connector
 
-conn = mysql.connector.connect(user='root', password='password', database='test')
 
-cursor = conn.cursor()
-# 创建user表:
-cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
-# 插入一行记录，注意MySQL的占位符是%s:
-cursor.execute('insert into user (id, name) values (%s, %s)', ('1', 'Michael'))
-print('rowcount =', cursor.rowcount)
-# 提交事务:
-conn.commit()
-cursor.close()
+def query_mysql(sql):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    return cursor.fetchall()
 
-# 运行查询:
-cursor = conn.cursor()
-cursor.execute('select * from user where id = %s', ('1',))
-values = cursor.fetchall()
-print(values)
-# 关闭Cursor和Connection:
-cursor.close()
-conn.close()
+
+def insert_mysql(sql, data=None):
+    cursor = conn.cursor()
+    if data is not None:
+        cursor.executemany(sql, data)
+    else:
+        cursor.execute(sql)
+    conn.commit()
+    return cursor.rowcount
+
+
+if __name__ == '__main__':
+    # config = configparser.ConfigParser()
+    # config.read('C:\\Workspace\\PycharmProjects\\python3_example\\xyzq\\jryqyj\\properties.ini')
+    # conn = mysql.connector.connect(user=config.get('database', 'jdbc.username'),
+    #                                password=config.get('database', 'jdbc.password'),
+    #                                database=config.get('database', 'jdbc.database'))
+    connect = mysql.connector.connect(user='root', password='By9216446o6', database='by_dylan')
+    query_mysql('select issr from t_risk_monit_issr_info;')
+    query_mysql("truncate table t_yj_news_middle;")
+    # 批量插入,data 为list(tuple) 类型 [(),()]
+    data = [(1, 2, 3), ('a', 'b', 'c')]
+    insert_result = insert_mysql("insert into t_yj_news_middle(title,url,keywords) values (%s,%s,%s);", data)
